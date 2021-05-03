ac33854d43ff5e5f0d07a9d6e3894457be7d5b6b
Adam <46168188+adamswagem@users.noreply.github.com>
2021-05-03 03:13:12 -0400

    App compiles and runs
    Fixed some gradle bs

--- app/src/main/java/com/example/composeweather/util/Constants.kt
+++ app/src/main/java/com/example/composeweather/util/Constants.kt
@@ -3,3 +3,7 @@ package com.example.composeweather.util
 const val API_KEY = "e8e98e12bdbf52acbf23acc3257c613a"
 
 const val BASE_URL ="https://api.openweathermap.org/data/2.5"
+
+const val NYC_LAT   = 40.7131
+
+const val NYC_LON  = -74.0072
